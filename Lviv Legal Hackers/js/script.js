172f5702f641982fc9327bb1f18956c531cfb207
Dmytro Zdebskyi <43642322+DimaZdebskyi@users.noreply.github.com>
2019-10-29 23:21:20 +0200

    Fix kostyl

--- Lviv Legal Hackers/js/script.js
+++ Lviv Legal Hackers/js/script.js
@@ -7,31 +7,3 @@ if (x.matches) {
      var x = window.matchMedia("(max-width: 990px)")
      myFunction(x) 
      x.addListener(myFunction) 
-
-
-// var $img = $('lviv-photo');
-
-// $img.load(function(){
-//     // удаляем атрибуты width и height
-//      $(this).removeAttr("width")
-//           .removeAttr("height")
-//           .css({ width: "", height: "" });
-
-//     // получаем заветные цифры
-// var width  = $(this).width();
-// var height = $(this).height();
-
-// });
-// showImgWH();
-// console.log('width= '+width(), 'height= '+height())
-
-
-let imgW;
-let imgH;
-function findWH(){
-     imgW=document.querySelector('#lviv-photo').width;
-     imgH=document.querySelector('#lviv-photo').height;
-     console.log('width='+imgW, 'height='+ imgH)
-     $('.photo-lviv-legal-hackers').css({'height': imgH })
-}
-findWH();
