94021216d2d574e63ed3b23dfa6a2dc2f7d1d429
romV2K <54354421+romV2K@users.noreply.github.com>
2019-10-29 18:48:26 +0200

    Photo of lviv and text onit was added

--- Lviv Legal Hackers/js/script.js
+++ Lviv Legal Hackers/js/script.js
@@ -1,9 +1,37 @@
 function myFunction(x) {
 if (x.matches) { 
      $("#navbar-collapse-items").addClass("collaps-items-set-middle");
-     // $(".nav-link").css("color","black");
+     
 }
 }
-   var x = window.matchMedia("(max-width: 990px)")
-   myFunction(x) // Call listener function at run time
-   x.addListener(myFunction) // Attach listener function on state changes
+     var x = window.matchMedia("(max-width: 990px)")
+     myFunction(x) 
+     x.addListener(myFunction) 
+
+
+// var $img = $('lviv-photo');
+
+// $img.load(function(){
+//     // удаляем атрибуты width и height
+//      $(this).removeAttr("width")
+//           .removeAttr("height")
+//           .css({ width: "", height: "" });
+
+//     // получаем заветные цифры
+// var width  = $(this).width();
+// var height = $(this).height();
+
+// });
+// showImgWH();
+// console.log('width= '+width(), 'height= '+height())
+
+
+let imgW;
+let imgH;
+function findWH(){
+     imgW=document.querySelector('#lviv-photo').naturalWidth;
+     imgH=document.querySelector('#lviv-photo').naturalHeight;
+     console.log('width='+imgW, 'height='+ imgH)
+     $('.photo-lviv-legal-hackers').css({'height': imgH })
+}
+findWH();
