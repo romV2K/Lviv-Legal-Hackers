e52effef3618e46404d004ef75ae7084122338e9
romV2K <54354421+romV2K@users.noreply.github.com>
2019-11-07 21:09:23 +0200

    added responsibility, and one ficha on js

--- Lviv Legal Hackers/js/script.js
+++ Lviv Legal Hackers/js/script.js
@@ -7,3 +7,11 @@ if (x.matches) {
      var x = window.matchMedia("(max-width: 990px)")
      myFunction(x) 
      x.addListener(myFunction) 
+
+
+
+$('#scroll').on('click', function() {
+     $('html,body').animate({scrollTop:$('#partners').offset().top+"px"},{duration:1E3});
+});
+
+
